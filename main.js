c384f0083dfab5e98a706c4a1dff773fd32d6e93
Guillaume <occuly.guillaume@gmail.com>
2019-01-15 22:06:08 +0100

    Event Handling

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ var app = new Vue({
     image: './assets/vmSocks-green.png',
     inStock: true,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-    variants: [{ variantId: 2234, variantColor: 'green' }, { variantId: 2235, variantColor: 'blue' }],
+    variants: [
+      { variantId: 2234, variantColor: 'green', variantImage: './assets/vmSocks-green.png' },
+      { variantId: 2235, variantColor: 'blue', variantImage: './assets/vmSocks-blue.png' },
+    ],
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1
+    },
+    updateProduct(variantImage) {
+      this.image = variantImage
+    }
   }
 })
