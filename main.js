fd3e43168d9d225152958a43ba6966de03922cb1
Guillaume <occuly.guillaume@gmail.com>
2019-01-15 22:52:52 +0100

    Components

--- main.js
+++ main.js
@@ -1,15 +1,54 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    product: 'Socks',
-    brand: 'Vue Mastery',
-    selectedVariant: 0,
-    details: ['80% cotton', '20% polyester', 'Gender-neutral'],
-    variants: [
-      { variantId: 2234, variantColor: 'green', variantImage: './assets/vmSocks-green.png', variantQuantity: 10 },
-      { variantId: 2235, variantColor: 'blue', variantImage: './assets/vmSocks-blue.png', variantQuantity: 0 },
-    ],
-    cart: 0,
+Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    }
+  },
+  template: `
+  <div class="product">
+    <div class="product-image">
+      <img :src='image'>
+    </div>
+
+    <div class="product-info">
+      <h1>{{ title }}</h1>
+      <p v-if='inStock'>In Stock</p>
+      <p v-else>Out of Stock</p>
+      <p>Shipping: {{ shipping }}</p>
+
+      <ul>
+        <li v-for='detail in details'> {{ detail }}</li>
+      </ul>
+
+      <div v-for='(variant, index) in variants' 
+        :key='variant.variantId'
+        class='color-box'
+        :style='{ backgroundColor: variant.variantColor }'
+        @mouseover='updateProduct(index)'>
+      </div>
+
+      <button @click='addToCart' :class='{ disabledButton: !inStock }' :disabled='!inStock'>Add to Cart</button>
+
+      <div class="cart">
+        <p>Cart( {{ cart }})</p>
+      </div>
+    </div>
+
+  </div>
+  `,
+  data() {
+    return {
+      product: 'Socks',
+      brand: 'Vue Mastery',
+      selectedVariant: 0,
+      details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+      variants: [
+        { variantId: 2234, variantColor: 'green', variantImage: './assets/vmSocks-green.png', variantQuantity: 10 },
+        { variantId: 2235, variantColor: 'blue', variantImage: './assets/vmSocks-blue.png', variantQuantity: 0 },
+      ],
+      cart: 0,
+    }
   },
   methods: {
     addToCart() {
@@ -26,8 +65,18 @@ var app = new Vue({
     image() {
       return this.variants[this.selectedVariant].variantImage
     },
-    inStock(){
+    inStock() {
       return this.variants[this.selectedVariant].variantQuantity
+    },
+    shipping() {
+      return this.premium ? 'Free' : 2.99
     }
+  }
+})
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    premium: true,
   }
 })
