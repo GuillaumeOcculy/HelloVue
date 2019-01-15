5d40747e4a920925bcad57760420b27ab7023690
Guillaume <occuly.guillaume@gmail.com>
2019-01-15 23:42:44 +0100

    Forms

--- main.js
+++ main.js
@@ -29,9 +29,22 @@ Vue.component('product', {
       </div>
 
       <button @click='addToCart' :class='{ disabledButton: !inStock }' :disabled='!inStock'>Add to Cart</button>
-
     </div>
 
+    <div>
+      <h2>Reviews</h2>
+      <p v-if='!reviews.length'>There are no reviews yet.</p>
+      
+      <ul>
+        <li v-for='review in reviews'>
+        <p>{{ review.name }}</p>
+        <p>{{ review.rating }}</p>
+        <p>{{ review.review }}</p>
+        </li>
+      </ul>
+    </div>
+
+    <product-review @review-submitted='addReview'></product-review>
   </div>
   `,
   data() {
@@ -44,6 +57,7 @@ Vue.component('product', {
         { variantId: 2234, variantColor: 'green', variantImage: './assets/vmSocks-green.png', variantQuantity: 10 },
         { variantId: 2235, variantColor: 'blue', variantImage: './assets/vmSocks-blue.png', variantQuantity: 0 },
       ],
+      reviews: [],
     }
   },
   methods: {
@@ -52,6 +66,9 @@ Vue.component('product', {
     },
     updateProduct(index) {
       this.selectedVariant = index
+    },
+    addReview(productReview) {
+      this.reviews.push(productReview)
     }
   },
   computed: {
@@ -66,6 +83,73 @@ Vue.component('product', {
     },
     shipping() {
       return this.premium ? 'Free' : 2.99
+    }
+  }
+})
+
+Vue.component('product-review', {
+  template: `
+  <form class="review-form" @submit.prevent="onSubmit">
+    <p v-if='errors.length'>
+      <b>Please correct the following error(s):</b>
+      <ul>
+        <li v-for='error in errors'>{{ error }}</li>
+      </ul>
+    </p>
+
+    <p>
+      <label for="name">Name:</label>
+      <input id="name" v-model="name" placeholder="name">
+    </p>
+    
+    <p>
+      <label for="review">Review:</label>      
+      <textarea id="review" v-model="review"></textarea>
+    </p>
+    
+    <p>
+      <label for="rating">Rating:</label>
+      <select id="rating" v-model.number="rating">
+        <option>5</option>
+        <option>4</option>
+        <option>3</option>
+        <option>2</option>
+        <option>1</option>
+      </select>
+    </p>
+        
+    <p>
+      <input type="submit" value="Submit">  
+    </p>    
+
+  </form>
+  `,
+  data() {
+    return {
+      name: null,
+      review: null,
+      rating: null,
+      errors: [],
+    }
+  },
+  methods: {
+    onSubmit() {
+      if (this.name && this.review && this.rating) {
+        let productReview = {
+          name: this.name,
+          review: this.review,
+          rating: this.rating,
+        }
+        this.$emit('review-submitted', productReview)
+        this.name = null
+        this.review = null
+        this.rating = null
+      }
+      else {
+        if (!this.name) this.errors.push('Name required.')
+        if (!this.review) this.errors.push('Review required.')
+        if (!this.rating) this.errors.push('Rating required.')
+      }
     }
   }
 })
